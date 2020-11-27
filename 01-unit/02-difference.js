c8885a25c2e3ad75faabe47fbf14ecf5714f4ce1
olivia_02 <robson.livi@gmail.com>
2020-11-27 11:47:11 -0500

    difference error checks made.

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,8 +1,12 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
-
+function difference(num1, num2) {
+  
+  if (num1 === undefined) num1 =0;
+  if (num2 === undefined) num2 =0;
+  
+  return num1 - num2;
 }
 
 // ==================================================
@@ -20,17 +24,24 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = difference(1, 1, 1, 1, 1);
+  if (result !== 0) throw new Error('Expected difference(1, 1, 1, 1, 1) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(1);
+  if (result !== 1) throw new Error('Expected difference(1) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = difference();
+  if (result !== 0) throw new Error('Expected difference() to be 0. Received: ' + result);
 
+  //--------
   console.log('All tests passed successfully.');
 
 // ==================================================
