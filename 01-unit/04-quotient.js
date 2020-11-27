4a17cdbb141dc93fcce50819fc9b76b839319b5c
olivia_02 <robson.livi@gmail.com>
2020-11-27 12:22:46 -0500

    Completed quotient; all unit calculator functions.

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,8 +1,16 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient() {
-
+function quotient(num1, num2) {
+ if (num1 === undefined){
+    return 0;
+  } else if (num2 === 0){
+    return "ERROR";
+  } else if (num2 === undefined){
+    return num1;
+  } else {
+    return num1 / num2;
+  }
 }
 
 // ==================================================
@@ -20,22 +28,28 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
-
+  var result = quotient(2, 0);
+  if (result !== "ERROR") throw new Error('Expected product(2, 0) to be ERROR. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = quotient(2, 2, 2, 2);
+  if (result !== 1) throw new Error('Expected product(2, 2, 2, 2) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = quotient(2);
+  if (result !== 2) throw new Error('Expected product(2, 0) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
+  
+  
   console.log('All tests passed successfully.');
 
 // ==================================================
