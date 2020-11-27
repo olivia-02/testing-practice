5ef86832a3918cce299916a583e56e3d71bf5af5
olivia_02 <robson.livi@gmail.com>
2020-11-27 11:43:16 -0500

    sum testing completed

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,8 +1,12 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum() {
-
+function sum(num1, num2) {
+  
+  if (num1 === undefined) num1 =0;
+  if (num2 === undefined) num2 =0;
+  
+  return num1 + num2
 }
 
 // ==================================================
@@ -20,17 +24,24 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = sum(1, 1, 1, 1, 1, 1, 1);
+  if (result !== 2) throw new Error('Expected sum(1, 1, 1, 1, 1, 1, 1) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = sum(1);
+  if (result !== 1) throw new Error('Expected sum(1) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum(0) to be 0. Received: ' + result);
 
+
+  //---------------------------------------------------
   console.log('All tests passed successfully.');
 
 // ==================================================
